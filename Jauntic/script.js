aad9dfbbf87ee4999a792d3cebad63d4ede3571b
Muzammil Alam <164224694+Omnicode786@users.noreply.github.com>
2025-07-18 16:25:56 +0500

    up

---
+++ Jauntic/script.js
@@ -0,0 +1,44 @@
+let icontheme = document.querySelector(".icontheme");
+let body = document.querySelector("body");
+let nav = document.querySelector(".nav");
+let navanchors = document.querySelectorAll(".theme1");
+let worktgt = document.querySelector(".main1 button");
+    let flag = 0;
+
+
+
+    icontheme.addEventListener("click",function(){
+        if (!flag) {
+          icontheme.src = "sun.png"
+          body.style.backgroundColor = "black";
+          body.style.color = "white";
+          nav.style.backgroundColor = "#000000ff";
+          navanchors.forEach(anchor => {
+      anchor.style.color = "#f8f9fa";
+      worktgt.style.color = "#f8f9fa";
+      worktgt.style.backgroundColor = "#88b8e7ff"
+    });
+
+
+    flag = 1;
+
+    }
+    else{
+        icontheme.src = "dark.png"
+          body.style.backgroundColor = "white";
+          body.style.color = "black";
+          nav.style.backgroundColor = "#f8f9fa";
+ navanchors.forEach(anchor => {
+      anchor.style.color = "#000707ff";
+      worktgt.style.color = "#000707ff";
+      worktgt.style.backgroundColor = "transparent"
+      worktgt.style.box-shadow
+
+    });
+
+
+    flag = 0;
+    }
+  
+})
+
